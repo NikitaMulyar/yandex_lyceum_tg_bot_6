768b0aa344938fec49642f4dc4b7688c40a56d01
NikitaMulyar <ni***ol@mail.ru>
2023-04-16 21:44:33 +0300

    10я задача

---
+++ server.py
@@ -0,0 +1,62 @@
+import logging
+from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
+from telegram import ReplyKeyboardMarkup, Bot, ReplyKeyboardRemove
+from config import BOT_TOKEN
+import requests
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+bot = Bot(BOT_TOKEN)
+API_KEY = 'AQVNzhfpiSw09Z_EBDWdUtal9XPJR_I5lVASpWlT'
+FOLDER_ID = 'b1go7l39akkvmdgv9j16'
+
+
+async def start(update, context):
+    kbrd = ReplyKeyboardMarkup([['RU -> EN', 'EN -> RU']])
+    await update.message.reply_text('Привет. Выбери направление перевода.', reply_markup=kbrd)
+    context.user_data['way'] = -1
+
+
+async def change_lang(update, context):
+    txt = update.message.text
+    if txt not in ['RU -> EN', 'EN -> RU']:
+        if not context.user_data.get('way') or context.user_data['way'] == -1:
+            await update.message.reply_text('Для начала выберите направление перевода.')
+            return
+        elif context.user_data['way'] == 'RU -> EN':
+            body = {"targetLanguageCode": "en", "languageCode": "ru", "texts": txt,
+                    "folderId": FOLDER_ID}
+            headers = {"Content-Type": "application/json", "Authorization": f"Api-Key {API_KEY}"}
+            response = requests.post('https://translate.api.cloud.yandex.net/translate/v2/translate',
+                                     json=body, headers=headers)
+            res = response.json()['translations'][0]['text']
+        else:
+            body = {"targetLanguageCode": "ru", "languageCode": "en", "texts": txt,
+                    "folderId": FOLDER_ID}
+            headers = {"Content-Type": "application/json", "Authorization": f"Api-Key {API_KEY}"}
+            response = requests.post(
+                'https://translate.api.cloud.yandex.net/translate/v2/translate',
+                json=body, headers=headers)
+            res = response.json()['translations'][0]['text']
+        await update.message.reply_text(f'Результат перевода:\n{res}')
+    else:
+        context.user_data['way'] = txt
+        await update.message.reply_text(f'Направление перевода обновлено на: {txt}.')
+
+
+def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    change_way = MessageHandler(filters.TEXT & ~filters.COMMAND, change_lang)
+    st = CommandHandler('start', start)
+    application.add_handler(st)
+    application.add_handler(change_way)
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
